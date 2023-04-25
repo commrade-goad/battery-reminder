a2bbc7a068f6f8297fc7c13dff6ecb962754417c
Fernando Perry <fernandoperry1234@gmail.com>
2023-04-25 17:22:59 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ DESTINATION=$2
 LINK="-lSDL2 -lSDL2_mixer -lmpg123"
 
 print_help(){
-    echo USAGE: \(1\) ./build.sh [gcc/clang]
-    echo        \(2\) ./build.sh [gcc/clang] [/path/and/name]
+    echo 'USAGE: (1) ./build.sh [gcc/clang]'
+    echo '       (2) ./build.sh [gcc/clang] [/path/and/name]'
     exit 1
 }
 
@@ -14,10 +14,12 @@ then
     DESTINATION="./batt-reminder"
 fi
 
-echo compiling...
 case $COMPILER in
     "gcc") g++ src/main.cpp $LINK -o $DESTINATION ;;
     "clang") clang++ -std=c++17 src/main.cpp -O2 $LINK -o $DESTINATION;;
     * ) print_help;;
 esac
-echo saved at \`$DESTINATION\`
+if [ $? == 0 ]
+    then
+        echo saved at \`$DESTINATION\`
+fi
